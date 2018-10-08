5f85433cc8a1f486f3565f8a8cbf3020ddb28930
gustavo esteves <gustavo.esteves@gmail.com>
2018-10-08 07:01:01 -0300

    change project name

--- AngularDefaultLoginProject/Models/ApplicationUser.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace AngularDefaultLoginProject.Models
-{
-    public class ApplicationUser : IdentityUser { }
-}
---
+++ AngularLogin/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AngularLogin.Models
+{
+    public class ApplicationUser : IdentityUser { }
+}
